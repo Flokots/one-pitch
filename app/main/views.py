2ab10683727f1735b2223bff9de415bfe24656c9
FloKots <florencekotohoyoh@gmail.com>
2022-05-10 06:35:45 +0300

    add user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, url_for, abort
+from flask import render_template,redirect, url_for, abort
 from flask_login import login_required
 
 from . import main
 from ..models import User, Comment
+from .forms import UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
@@ -12,6 +14,36 @@ def index():
 
   title = 'One Pitch'
   return render_template('index.html', title=title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username=uname).first()
+
+  if user is None:
+    abort(404)
+
+  return render_template("profile/profile.html", user=user)
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+  user = User.query.filter_by(username=uname).first()
+  
+  if user is None:
+    abort(404)
+  
+  form = UpdateProfile()
+
+  if form.validate_on_submit():
+    user.bio = form.bio.data
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('main.profile', uname=user.username))
+  return render_template('profile/update.html', form=form)
+
 
 
 @main.route('/categories')
@@ -43,4 +75,3 @@ def pitch(id):
   return render_template(url_for('pitch.html'), id=id)
 
 
-# @main.route('/pitch/<int:id>/comment/')
